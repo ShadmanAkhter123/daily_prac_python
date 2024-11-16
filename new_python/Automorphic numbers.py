c15530ec18d3166a45b9bf4fb5b7607784bdbe2d
ShadmanAkhter123 <shadman.akhter123@gmail.com>
2024-11-16 23:52:38 +0600

    failed today automorphic...4th day

--- new_python/Automorphic numbers.py
+++ new_python/Automorphic numbers.py
@@ -5,20 +5,18 @@ n= int(input("enter the number: "))
 
 c = n
 r=0
+s=n*n
+y=s
+x= []
 while c >= 1:
     b = c % 10
-    r=r+
+    a= y%10
+
+    x.append(a)
+    y=int(y/10)
     c = c // 10
+x.reverse()
 
 
 
-print(r)
-
-
-
-
-
-
-
-
-
+print(x)
