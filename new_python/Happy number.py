ef5d17dd1aa3bc3f1573dd3730b128f42a6b7609
ShadmanAkhter123 <shadman.akhter123@gmail.com>
2024-12-02 23:56:40 +0600

    after so long...failed happy num

--- new_python/Happy number.py
+++ new_python/Happy number.py
@@ -1,3 +1,15 @@
-#happy number
+# happy number
 
-for example 19
+n = int(input("Enter the number: "))
+
+a= n
+
+while(a==1)
+
+
+    b= int(a/10)
+    c= a%10
+
+    a= b**2+c**2
+
+print(f"{b}, {c}    {a}")
